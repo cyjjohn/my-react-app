8965c861a963506dfd6d4a82de3f2a541a334789
cyjjo <cyjjohn@hotmail.com>
2024-01-10 16:37:29 +0800

    first commit

---
+++ .prettierrc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  useTabs: false,
+  printWidth: 100, //单行长度
+  tabWidth: 2, //缩进长度
+  semi: false, //句末使用分号
+  singleQuote: true, //使用单引号
+  arrowParens: 'avoid', //单参数箭头函数省略圆括号
+  proseWrap: 'preserve', //换行方式
+  endOfLine: 'lf', //行结束符
+  overrides: [
+    {
+      files: '*.md',
+      options: {
+        printWidth: 70,
+        useTabs: false,
+        trailingComma: 'none',
+        arrowParens: 'avoid',
+        proseWrap: 'never',
+      },
+    },
+    {
+      files: '*.{json,babelrc,eslintrc,remarkrc,prettierrc}',
+      options: {
+        useTabs: false,
+      },
+    },
+  ],
+}
---
+++ vite.config.ts
@@ -0,0 +1,42 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import { visualizer } from 'rollup-plugin-visualizer'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src',
+      css: '/src/assets/css',
+      components: '/src/components',
+      utils: '/src/utils',
+      pages: '/src/pages',
+    },
+  },
+  server: {
+    port: 3000,
+    open: false,
+    proxy: {
+      //代理实现跨域
+      '^/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true, //开启代理
+      },
+    },
+  },
+  esbuild: {
+    pure: ['console.log'], //只删除console.log
+    drop: ['debugger'],
+  },
+  build: {
+    rollupOptions: {
+      plugins: [visualizer({ open: true })], //打包分析器
+      output: {
+        chunkFileNames: 'js/[name]-[hash].js', // 引入文件名的名称
+        entryFileNames: 'js/[name]-[hash].js', // 包的入口文件名称
+        assetFileNames: '[ext]/[name]-[hash].[ext]', // 资源文件像 字体，图片等
+      },
+    },
+  },
+})
