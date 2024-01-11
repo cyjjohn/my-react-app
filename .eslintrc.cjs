283612a7967dc55d13b73bc1fabdadfe7ced7eac
cyjjo <cyjjohn@hotmail.com>
2024-01-11 13:36:36 +0800

    chore: commit-lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,8 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
     'plugin:react-hooks/recommended',
     'plugin:prettier/recommended',
   ],
@@ -12,5 +13,14 @@ module.exports = {
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: [
+      './tsconfig.json',
+      './tsconfig.node.json', // 针对 Node 的 tsconfig
+    ],
+    tsconfigRootDir: __dirname,
   },
 }
